3370f38931b70617c4a480a1acd3fdaaf364198b
Aalam2000 <a9160371537@gmail.com>
2025-12-06 19:35:13 +0400

    ЧатБот 4

--- chatgpt_ui/admin.py
+++ chatgpt_ui/admin.py
@@ -1,3 +1,22 @@
+# chatgpt_ui/admin.py
 from django.contrib import admin
+from .models import ChatSession, ChatMessage
 
-# Register your models here.
+
+@admin.register(ChatSession)
+class ChatSessionAdmin(admin.ModelAdmin):
+    list_display = ("id", "telegram_id", "user", "is_active", "created_at")
+    search_fields = ("telegram_id",)
+    list_filter = ("is_active",)
+    ordering = ("-created_at",)
+
+
+@admin.register(ChatMessage)
+class ChatMessageAdmin(admin.ModelAdmin):
+    list_display = ("id", "session", "role", "short_text", "created_at")
+    list_filter = ("role", "created_at")
+    search_fields = ("content",)
+
+    def short_text(self, obj):
+        return obj.content[:50]
+
