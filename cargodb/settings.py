ac80e9aad4abb684ed08272ea5a174dde67e324b
Aalam2000 <a9160371537@gmail.com>
2025-12-24 11:55:24 +0400

    АВТОДЕПЛОЙ 3

--- cargodb/settings.py
+++ cargodb/settings.py
@@ -118,6 +118,7 @@ TEMPLATES = [
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'web/static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
